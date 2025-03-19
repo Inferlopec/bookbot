bed4325b87e080715f717aa13ac0eec20ce952e7
Inferlopec <ilja.golubevs@gmail.com>
2025-03-19 17:13:05 +0200

    adding number of characters dictionary

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_of_characters
 
-
-
-
-    
 get_num_words()
+
+char_dict = get_num_of_characters()
+print(char_dict)
+
+
     
--- stats.py
+++ stats.py
@@ -1,9 +1,22 @@
 def get_book_text(path_to_file):
     with open(path_to_file, 'r') as file:
-        return file.read()
+        return file.read().lower()
 
 def get_num_words():
     path = "books/frankenstein.txt"
     book_text = get_book_text(path)
     num_words = len(book_text.split())
     print(f"{num_words} words found in the document")
+
+def get_num_of_characters():
+    path = "books/frankenstein.txt"
+    book_text = get_book_text(path)
+    char_dict = {}
+    for char in book_text:
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    return char_dict
+    
+    
