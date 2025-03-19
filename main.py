3c72263c813594119f1b058d4aa47f9efbce8afd
Inferlopec <ilja.golubevs@gmail.com>
2025-03-19 17:50:25 +0200

    add user input/remove hard coded locations

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from stats import get_num_words, get_num_of_characters
+from stats import get_num_words, get_num_of_characters, char_dict_sort
+import sys
 
-get_num_words()
+if len(sys.argv) != 2:
+    print("Usage: python main.py <path_to_book>")
+    sys.exit(1)
 
-char_dict = get_num_of_characters()
-print(char_dict)
+path_to_book = sys.argv[1]
+
+num_words = get_num_words(path_to_book)
+char_dict = get_num_of_characters(path_to_book)
+sorted_char_dict = char_dict_sort(char_dict)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path_to_book}...")
+print("----------- Word Count ----------")
+print(f"Found {num_words} total words")
+print("--------- Character Count -------")
+for char, count in sorted_char_dict.items():
+    print(f"{char}: {count}")
+
 
 
     
--- stats.py
+++ stats.py
@@ -2,21 +2,24 @@ def get_book_text(path_to_file):
     with open(path_to_file, 'r') as file:
         return file.read().lower()
 
-def get_num_words():
-    path = "books/frankenstein.txt"
-    book_text = get_book_text(path)
+def get_num_words(path_to_book):
+    book_text = get_book_text(path_to_book)
     num_words = len(book_text.split())
-    print(f"{num_words} words found in the document")
+    return num_words
 
-def get_num_of_characters():
-    path = "books/frankenstein.txt"
-    book_text = get_book_text(path)
+def get_num_of_characters(path_to_book):
+    book_text = get_book_text(path_to_book)
     char_dict = {}
     for char in book_text:
+        if char.isalpha():
             if char in char_dict:
                 char_dict[char] += 1
             else:
                 char_dict[char] = 1
     return char_dict
+
+def char_dict_sort(char_dict):
+    sorted_char_dict = dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+    return sorted_char_dict
     
     
